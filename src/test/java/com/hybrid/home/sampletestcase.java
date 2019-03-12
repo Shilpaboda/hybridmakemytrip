f8e6b4fe359f59b973417d72dbdac43bf7c6267c
Shilpa <shilpa@Arvinds-MacBook-Air.local>
2019-03-12 18:41:40 +0000

    testing

---
+++ src/test/java/com/hybrid/home/sampletestcase.java
@@ -0,0 +1,11 @@
+package com.hybrid.home;
+
+import org.testng.annotations.Test;
+
+public class sampletestcase {
+
+    @Test
+    public void welcome(){
+        System.out.println("welcome");
+    }
+}
